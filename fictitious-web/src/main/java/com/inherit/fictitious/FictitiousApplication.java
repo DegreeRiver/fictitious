6f567a82d9d651ce4c56fb41609b124a9ce08c1d
dubaolong <649001717@qq.com>
2018-08-05 23:24:12 +0800

    20180805 commit message :代码初始化一波

--- fictitious-web/src/main/java/com/inherit/fictitious/FictitiousApplication.java
+++ fictitious-web/src/main/java/com/inherit/fictitious/FictitiousApplication.java
@@ -2,8 +2,9 @@ package com.inherit.fictitious;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FictitiousApplication {
 
 	public static void main(String[] args) {
