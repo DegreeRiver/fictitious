76d617b2b6442bfb33bcc180308622d291f1862e
dubaolong <649001717@qq.com>
2018-08-05 23:05:59 +0800

    Initial commit

---
+++ src/main/java/com/inherit/fictitious/FictitiousApplication.java
@@ -0,0 +1,12 @@
+package com.inherit.fictitious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FictitiousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FictitiousApplication.class, args);
+	}
+}
