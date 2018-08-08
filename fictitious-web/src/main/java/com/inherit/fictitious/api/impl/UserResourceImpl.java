1b5916681a15f8b12d664630dadb2c24e4bde324
dubaolong <649001717@qq.com>
2018-08-08 09:40:08 +0800

    20180808 commit message :代码初始化一波

--- fictitious-web/src/main/java/com/inherit/fictitious/api/impl/UserResourceImpl.java
+++ fictitious-web/src/main/java/com/inherit/fictitious/api/impl/UserResourceImpl.java
@@ -1,12 +1,14 @@
 package com.inherit.fictitious.api.impl;
 
 import com.fictitious.inherit.api.UserResource;
+import org.springframework.stereotype.Controller;
 
 /**
  * @Author dubaolong
  * @Date 2018/8/8 09:37
  * @PackageName com.inherit.fictitious.api.impl
  */
+@Controller("userResource")
 public class UserResourceImpl implements UserResource {
 
     @Override
